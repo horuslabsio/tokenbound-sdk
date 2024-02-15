0591fc88f2de97aae38db20e06447bb2cb74ba67
Darlington Nnam <Darlingtonnnam@gmail.com>
2024-02-15 02:48:41 +0100

    cleanup src directory

--- src/types/walletClient.ts
+++
@@ -1,4 +0,0 @@
-export interface WalletClient {
-    address: string
-    privateKey: string
-}
--- src/utils/account.ts
+++
@@ -1,8 +0,0 @@
-import { Account } from "starknet"
-import { getProvider } from "./provider"
-import { WalletClient } from "../types/walletClient"
-
-export function accountClient(jsonRPC: string, walletClient?: WalletClient): Account {
-    const account = new Account(getProvider(jsonRPC), walletClient!.address, walletClient!.privateKey)
-    return account
-} 
