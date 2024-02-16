5d50fd8d209291298f9207c0f221f7524eedfb04
Darlington02 <darlingtonnnam@gmail.com>
2024-02-16 07:31:01 +0100

    chore: implement tokenboundAccount class

---
+++ connector/src/connector/constants/index.ts
@@ -0,0 +1,3 @@
+export const registryAddress: string = "0x1b0ef7a47d9db8652f8a9010ecaf3e6537442bfab3afed13449b571fa1da37a";
+export const implementationAddress: string = "0x011bc9fabead984d714cf82ec46ffa23f4558f27ae73561542fed9fa8fb510ae";
+export const jsonRPC = process.env.REACT_APP_RPC_URL!;
