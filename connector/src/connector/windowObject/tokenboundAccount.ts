e757834c07b0ad5edf97184f6d41a603e843c0c1
Darlington02 <darlingtonnnam@gmail.com>
2024-03-07 02:24:14 +0100

    chore: assign parentAccount as signer

--- connector/src/connector/windowObject/tokenboundAccount.ts
+++ connector/src/connector/windowObject/tokenboundAccount.ts
@@ -1,45 +1,19 @@
-import { 
-    Signature,
+import {
     Account,
     AccountInterface,
     ProviderInterface,
-    SignerInterface,
     Call,
     CallData,
     RawArgs
 } from "starknet";
 
-class UnimplementedSigner implements SignerInterface {
-    async getPubKey(): Promise<string> {
-        throw new Error("Method not implemented");
-    }
-
-    async signMessage(): Promise<Signature> {
-        throw new Error("Method not implemented");
-    }
-
-    async signTransaction(): Promise<Signature> {
-        throw new Error("Method not implemented");
-    }
-
-    async signDeclareTransaction(): Promise<Signature> {
-        throw new Error("Method not implemented");
-    }
-
-    async signDeployAccountTransaction(): Promise<Signature> {
-        throw new Error("Method not implemented");
-    }
-}
-
  export class TokenboundAccount extends Account implements AccountInterface {
-    public signer = new UnimplementedSigner();
-
     constructor(
         provider: ProviderInterface,
         public address: string,
         public parentAccount: AccountInterface
     ) {
-        super(provider, address, new UnimplementedSigner);
+        super(provider, address, parentAccount.signer);
     }
 
     override execute = async (
